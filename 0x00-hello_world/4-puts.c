10b7dd10047b2ebd2cfe726154360c511df79314
Ay-show <ayyobello6@gmail.com>
2022-09-08 16:38:58 -0700

    task 2-4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+
+/**
+* main - print the string in the function
+*
+* Description: using the main function
+* this program prints "programing is the like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+	puts(*\"programing is the like building a multilingual puzzle");
+	return (0);
+}
