23fc726d81147b5693356c665458a6d09740f774
Ay-show <ayyobello6@gmail.com>
2022-09-08 16:46:53 -0700

    task 4 correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 * main - print the string in the function
 *
 * Description: using the main function
-* this program prints "programing is the like building a multilingual puzzle
+* this program prints "programming is the like building a multilingual puzzle
 * Return: 0
 */
 int main(void)
 {
-	puts(*\"programing is the like building a multilingual puzzle");
+	puts(*\"programming is the like building a multilingual puzzle");
 	return (0);
 }
