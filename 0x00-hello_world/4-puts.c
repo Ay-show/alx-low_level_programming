4117aaa0b709c5cfe4be26551392042d8513a677
Ay-show <ayyobello6@gmail.com>
2022-09-08 17:22:37 -0700

    4-puts.c 2correctn

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,21 @@
-
 /**
-* main - print the string in the function
-*
-* Description: using the main function
-* this program prints "programming is the like building a multilingual puzzle
-* Return: 0
-*/
+ *
+ * * main - Printing
+ *
+ * *
+ *
+ * * Return: Always 0 (Successful)
+ *
+ * */
+
 int main(void)
+
 {
-	puts(*\"programming is the like building a multilingual puzzle");
+
+	puts("\"Programming is like building a multilingual
+
+			puzzle");
+
 	return (0);
+
 }
